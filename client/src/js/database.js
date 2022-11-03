49c0a4ae485be71f61e064cb7323decc864c6988
RyanNazario0357 <Ryan0357@icloud.com>
2022-11-03 19:01:07 -0400

    add logic for a method that gets content from db

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -23,6 +23,15 @@ const result = await request;
 console.log('🚀 - data saved to the database', result);
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-
+export const getDb = async () => {
+  console.log('GET from the database');
+  
+const contactDb = await openDB('jate_db', 1);
+const tx = contactDb.transaction('jate_db', 'readonly');
+const store = tx.objectStore('jate');
+const request = store.getAll();
+const result = await request;
+console.log('result.value', result);
+return result;
+};
 initdb();
