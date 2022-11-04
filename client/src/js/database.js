7a84831347c64e05ca702372c241ec9298309d97
RyanNazario0357 <Ryan0357@icloud.com>
2022-11-03 20:02:34 -0400

    added code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -23,5 +23,13 @@ const initdb = async () =>
   };
 
 //Put
-
+export const putDb = async (id, value) => {
+  console.log('PUT request to update the jateDB');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const objStore = tx.objectStore('jate');
+  const req = objStore.put({ id: id, value: value })
+  const res = await req;
+  console.log('data saved to the jateDB', res);
+};
 initdb();
