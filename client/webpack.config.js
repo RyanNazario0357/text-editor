c0983822e1f588b836557af4d82017c203757928
RyanNazario0357 <Ryan0357@icloud.com>
2022-11-03 20:38:06 -0400

    messing with code

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -29,20 +29,23 @@ module.exports = () => {
       }),
 
       new WebpackPwaManifest({
-        name: 'Jate-code-editor-Application',
+        fingerprints: false,
+        inject: true,
+        name: 'text-editor',
         short_name: 'JATE',
-        description: 'Functioning code editor!',
-        background_color: '#D3D3D3',
-        theme_color: '#D3D3D3',
-        start_url: './',
-        publicPath: './',
-        icons: [{
-          src: path.resolve('src/images/logo.png'),
-          sizes: [96, 128, 192, 256, 384, 512],
-          destination: path.join('assets', 'icons'),
-        }],
+        description: 'Just Another Text Editor!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
       }),
-
     ],
     module: {
       rules: [{
